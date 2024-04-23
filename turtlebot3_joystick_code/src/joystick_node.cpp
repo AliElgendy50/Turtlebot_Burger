3278fdda4ce80fba6fb6979f5073b04732b092d9
Marc Hany <101137476+Marc-Hany@users.noreply.github.com>
2024-04-23 13:25:25 +0200

    Update joystick_node.cpp
    Mapping Edit

--- turtlebot3_joystick_code/src/joystick_node.cpp
+++ turtlebot3_joystick_code/src/joystick_node.cpp
@@ -4,10 +4,11 @@
 #include "turtlesim/Pose.h"
 #include <math.h>
 
+//Initialize data
 int x = 0; 
 int y = 0;
-int max_speed = 4;
 
+//Initialize Node and publisher
 ros::NodeHandle nh; 
 
 geometry_msgs::Twist joystick_msg; 
@@ -24,12 +25,28 @@ void setup()
 }
 void loop() {
 
-  x = get_x_pos();
-  y = get_y_pos();
+  //Read data
+  x = get_x_pos()-508;
+  y = get_y_pos()-510;
+  // map x and y values to be x(-5,5) and y(-77,77)
+  if(x>0)
+  {
+    x=map(x,0,515,0,5);
+  }
+  if(x<0)
+  {
+    x=map(x,0,-508,0,-5);
+  }
+  if(y>0)
+  {
+    y=map(y,0,513,0,77);
+  }
+  if(y<0)
+  {
+    y=map(y,0,-510,0,-77);
+  }
 
-  x = map(x, 0, 1023, -max_speed, max_speed) +1; 
-  y = map(y, 0, 1023, max_speed, -max_speed) -1;
-
+  //Publish data
   joystick_msg.linear.x = x;
   joystick_msg.angular.z = y;
 
