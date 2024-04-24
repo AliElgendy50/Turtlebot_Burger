c9bcaff55b5e98b237dbe7872ef04ba384476d26
Marc Hany <101137476+Marc-Hany@users.noreply.github.com>
2024-04-24 19:04:42 +0200

    Update visualisation.py
    updated sensitivity

--- turtlebot3_laptop_code/scripts/visualisation.py
+++ turtlebot3_laptop_code/scripts/visualisation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import rospy
 from nav_msgs.msg import Odometry
-from tf.transformations import euler_from_quaternion
 import numpy as np
+from tf.transformations import euler_from_quaternion
+import time
 import matplotlib.pyplot as plt
 
 class DifferentialDriveRobot:
@@ -12,8 +13,6 @@ class DifferentialDriveRobot:
         self.x = 0.0 # initial x position
         self.y = 0.0 # initial y position
         self.theta = 0.0 # initial orientation
-        self.vx = 0.0 # initial linear velocity
-        self.vtheta = 0.0 # initial angular velocity
         self.history = {'x': [], 'y': [], 'theta': []}
 
     def odom_callback(self, msg):
@@ -22,10 +21,6 @@ class DifferentialDriveRobot:
         self.y = msg.pose.pose.position.y
         orientation_q = msg.pose.pose.orientation
         _, _, self.theta = euler_from_quaternion([orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w])
-        
-        # Extract linear and angular velocities
-        self.vx = msg.twist.twist.linear.x
-        self.vtheta = msg.twist.twist.angular.z
 
         # Append current pose to history
         self.history['x'].append(self.x)
@@ -33,29 +28,46 @@ class DifferentialDriveRobot:
         self.history['theta'].append(self.theta)
 
     def plot_robot(self):
-        # Plot robot
-        plt.plot(self.x, self.y, 'ro')
-        plt.quiver(self.x, self.y, np.cos(self.theta), np.sin(self.theta))
+        plt.plot(self.history['x'], self.history['y'], 'ro')
+        arrow_direction_x = -np.sin(self.theta)
+        arrow_direction_y = np.cos(self.theta)
+        plt.quiver(self.history['x'], self.history['y'], arrow_direction_x, arrow_direction_y)
         plt.xlabel('X')
         plt.ylabel('Y')
         plt.title('Differential Drive Robot Simulation')
         plt.axis('equal')
         plt.grid(True)
 
+        # Increase graph sensitivity
+        plt.xlim(self.x - 0.1, self.x + 0.1)
+        plt.ylim(self.y - 0.1, self.y + 0.1)
+             
+            
+        # plt.xlim(0.01, None)
+        # plt.ylim(0.01, None)
+        # plt.plot(self.x, self.y, 'ro')
+        # plt.quiver(self.x, self.y, np.cos(self.theta), np.sin(self.theta))
+        # plt.xlabel('X')
+        # plt.ylabel('Y')
+        # plt.title('Differential Drive Robot Simulation')
+        # plt.axis('equal')
+        # plt.grid(True)
+
 def main():
     # Initialize ROS node
     rospy.init_node('robot_visualization', anonymous=True)
 
     # Create instance of DifferentialDriveRobot class
-    robot = DifferentialDriveRobot(wheel_radius=0.05, wheel_base=0.2)
+    robot = DifferentialDriveRobot(wheel_radius=0.04, wheel_base=0.13)
 
     # Subscribe to the Odometry topic published by robot state publisher
     rospy.Subscriber('/odom', Odometry, robot.odom_callback)
 
     # Simulation loop
     while not rospy.is_shutdown():
+        # time.sleep(1)  # Wait for 1 second
         robot.plot_robot()
-        plt.pause(0.1)
+        plt.pause(0.01)
 
     plt.show()
 
