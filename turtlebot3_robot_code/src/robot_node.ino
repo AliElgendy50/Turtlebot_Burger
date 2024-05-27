6af45d92074069053ae001380b2ab78329494445
Marc Hany <101137476+Marc-Hany@users.noreply.github.com>
2024-05-28 01:52:09 +0300

    Add files via upload
    ESP code

--- turtlebot3_robot_code/src/robot_node.ino
+++ turtlebot3_robot_code/src/robot_node.ino
@@ -5,6 +5,23 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <sensor_msgs/Imu.h>
+#include <nav_msgs/Odometry.h>
+#include <ESP32Servo.h>
+#include <Ultrasonic.h>
+
+
+/*********************************Define the servo data*********************************/
+#define Servo_pin 27
+Servo myservo;
+int angle = 0;
+char flag=0;
+
+/*********************************Define Ultrasonic data*********************************/
+const int trigPin1 = 2;  // Replace with your GPIO pin
+const int echoPin1 = 15; // Replace with your GPIO pin
+
+Ultrasonic ultrasonic(trigPin1, echoPin1);
 
 /*********************************Robot Details***************************************/
 
@@ -15,6 +32,7 @@ const float wheelBase=0.13;
 /**********************************Define variables for time tracking*****************/
 unsigned long previousMillis = 0; 
 const long interval = 1000; 
+const long interval2 = 50;
 unsigned long currentMillis = 0;
 
 /******************************Define a class for Bluetooth hardware******************/
@@ -61,13 +79,15 @@ Adafruit_MPU6050 mpu;
 ros::NodeHandle_<BluetoothHardware> nh;
 
 /************************************************************************************/
-
-// Forward declaration of the robot callback function
-void robot_callback(const geometry_msgs::Twist&);
-
-
 // Create a pointer to a DifferentialDriveRobot object
 DifferentialDriveRobot *my_robot;
+/// Robot callback function
+void robot_callback(const geometry_msgs::Twist& msg) 
+{
+
+  my_robot->move(msg.linear.x, msg.angular.z);
+  
+}
 
 
 // Create a ROS subscriber
@@ -78,6 +98,17 @@ ros::Subscriber <geometry_msgs::Twist> sub("/turtle1/cmd_vel", &robot_callback);
 geometry_msgs::Twist cmd_msg;
 ros::Publisher cmd_pub("cmd_vel", &cmd_msg);
 
+// Publish IMU data
+sensor_msgs::Imu imu_msg;
+ros::Publisher imu_pub("imu", &imu_msg);
+
+// Publish Odometry data
+nav_msgs::Odometry odom_msg;
+ros::Publisher odom_pub("odom", &odom_msg);
+
+// Publish Ultrasonic data
+geometry_msgs::Twist ultrasonic_msg;
+ros::Publisher sensor_pub("ultrasonic_data", &ultrasonic_msg);
 
 /********************************** Function to handle encoder A************************************/
 
@@ -120,14 +151,15 @@ void handleEncoder_B()
   lastEncoderState_B = currentState_B;
 }
 
-
 /**********************************Set Up Function***********************************/
 
 void setup()
 {
   // Start the Bluetooth connection
   SerialBT.begin("ESP32_BT"); //Bluetooth device name
-  // Initialize the robot
+  myservo.attach(Servo_pin);
+  myservo.write(0);
+  // // Initialize the robot
   my_robot = new DifferentialDriveRobot(
     new DCMotor(IN1, IN2, ENA),
     new DCMotor(IN3, IN4,ENB));
@@ -135,7 +167,7 @@ void setup()
   // Update the robot's parameters
   my_robot->updateParameters(0.2, 1);
 
-  // Try to initialize the MPU6050
+  // // Try to initialize the MPU6050
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -149,6 +181,7 @@ void setup()
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   // set filter bandwidth to 21 Hz
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
 
   // Set encoder pins as inputs
   pinMode(ENCODER_LEFT_MOTOR_A, INPUT);
@@ -167,6 +200,9 @@ void setup()
   // Initialize the ROS node, advertise the publisher, and subscribe to the subscriber
   nh.initNode();
   nh.advertise(cmd_pub);
+  nh.advertise(imu_pub);
+  nh.advertise(odom_pub);
+  nh.advertise(sensor_pub);
   nh.subscribe(sub);
 
 }
@@ -178,14 +214,42 @@ void loop()
   // Update the current time 
   currentMillis = millis();
 
-  //get mpu readings
+  //get mpu readings#!/usr/bin/env python3
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
+  // Servo control for scanning
+ 
 
-
+ if(currentMillis - previousMillis >= interval2)
+ {
+  if(flag==0)
+  {
+    angle+=1;
+    myservo.write(angle);
+    if(angle>=180)
+    {
+      flag=1;
+    }
+  }
+  else if(flag==1)
+  {
+    angle-=1;
+    myservo.write(angle);
+    if(angle==0)
+    {
+      flag=0;
+    }
+  }
+  previousMillis = currentMillis;
+ }
  // If the interval has passed
  if(currentMillis - previousMillis >= interval)
  {
+  ultrasonic_msg.linear.x = ultrasonic.read();
+  // ultrasonic_msg.data[1] = distance2;
+  // printf("Ultrasonic Sensor 1: %f", distance1);
+  sensor_pub.publish(&ultrasonic_msg);
+
   if(int(a.acceleration.x*10)>-10 && int(a.acceleration.x*10)<10 && int(a.acceleration.y*10)>-10 && int(a.acceleration.y*10)<10)
   {
     cmd_msg.linear.x = 0;
@@ -203,6 +267,7 @@ void loop()
 
 
   }
+   
     //publishe angular velocity from imu
     cmd_msg.angular.z = g.gyro.z-0.05+0.019;
 
@@ -212,6 +277,29 @@ void loop()
     // Reset the encoder counts
     encoderCount_A=0;
     encoderCount_B=0;
+
+    // Read the ultrasonic sensor data
+
+   
+    // imu_msg.header.stamp = nh.now();
+    // imu_msg.header.frame_id = "imu_link";
+    // imu_msg.orientation_covariance[0] = -1; // Indicates that orientation data is not provided
+    // imu_msg.angular_velocity.x = g.gyro.x;
+    // imu_msg.angular_velocity.y = g.gyro.y;
+    // imu_msg.angular_velocity.z = g.gyro.z;
+    // imu_msg.linear_acceleration.x = a.acceleration.x;
+    // imu_msg.linear_acceleration.y = a.acceleration.y;
+    // imu_msg.linear_acceleration.z = a.acceleration.z;
+    // imu_pub.publish(&imu_msg);
+
+    
+    // // Publish simplified Odometry data
+    // odom_msg.header.stamp = nh.now();
+    // odom_msg.header.frame_id = "odom";
+    // odom_msg.child_frame_id = "base_link";
+    // odom_msg.twist.twist.linear.x = encoder_linearVelocity;
+    // odom_msg.twist.twist.angular.z = g.gyro.z;
+    // odom_pub.publish(&odom_msg);
 
     // Update the previous time
     previousMillis = currentMillis; 
@@ -224,11 +312,3 @@ void loop()
 }
 
 
-// Robot callback function
-void robot_callback(const geometry_msgs::Twist& msg) 
-{
-
-  // Move the robot
-	my_robot->move(msg.linear.x, msg.angular.z);
-  
-}
