4302678fd9ddecba92abdac4863250ccaa84ecdc
AliElgendy50 <104139348+AliElgendy50@users.noreply.github.com>
2024-06-01 16:08:27 +0300

    Update robot_node.ino

--- turtlebot3_robot_code/src/robot_node.ino
+++ turtlebot3_robot_code/src/robot_node.ino
@@ -1,119 +1,119 @@
-/**********************Include the necessary libraries********************************/
 #include "DifferentialDriveRobot.h"
 #include <BluetoothSerial.h>
 #include "Encoder.h"
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <sensor_msgs/Imu.h>
+#include <nav_msgs/Odometry.h>
+#include <ESP32Servo.h>
+#include <Ultrasonic.h>
+#include <std_msgs/String.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/Float64.h>
 
-/*********************************Robot Details***************************************/
+#define Servo_pin 27
+Servo myservo;
+int angle = 0;
+char flag=0;
 
-// Define the robot's wheel radius and wheel base
-const float wheelRadius=0.04;
-const float wheelBase=0.13;
+#define TRIGGER_PIN1 2
+#define ECHO_PIN1 35
+Ultrasonic ultrasonic1(TRIGGER_PIN1, ECHO_PIN1);
 
-/**********************************Define variables for time tracking*****************/
+const float wheelRadius = 0.04;
+const float wheelBase = 0.13;
+
 unsigned long previousMillis = 0; 
 const long interval = 1000; 
+const long interval2 = 50;
 unsigned long currentMillis = 0;
 
-/******************************Define a class for Bluetooth hardware******************/
 BluetoothSerial SerialBT;
+
 class BluetoothHardware {
 public:
   BluetoothHardware() : connected(false) {}
 
-  void init() 
-  {
-    // Start the Bluetooth connection
+  void init() {
     SerialBT.begin("ESP32_BT");
   }
 
-   int read() 
-   {
-     // Read a byte from the Bluetooth connection
-     if (SerialBT.available())
-       return SerialBT.read();
-     else
-       return -1;
-   }
+  int read() {
+    if (SerialBT.available())
+      return SerialBT.read();
+    else
+      return -1;
+  }
 
-
-  void write(uint8_t* data, int length) 
-  {
-  // Write data to the Bluetooth connection
-     for(int i=0; i<length; i++)
-       SerialBT.write(data[i]);
-   }
-
+  void write(uint8_t* data, int length) {
+    for(int i=0; i<length; i++)
+      SerialBT.write(data[i]);
+  }
 
   unsigned long time() {
-    // Return the current time in milliseconds
     return millis();
   }
 
   bool connected;
 };
 
-//Create an instance for mpu
 Adafruit_MPU6050 mpu;
-// Create a ROS node handle
 ros::NodeHandle_<BluetoothHardware> nh;
 
-/************************************************************************************/
-
-// Forward declaration of the robot callback function
-void robot_callback(const geometry_msgs::Twist&);
-
-
-// Create a pointer to a DifferentialDriveRobot object
 DifferentialDriveRobot *my_robot;
 
+geometry_msgs::Twist(lol_msg);
+ros::Publisher lol_pub("cmd_vel", &lol_msg);
 
-// Create a ROS subscriber
-ros::Subscriber <geometry_msgs::Twist> sub("/turtle1/cmd_vel", &robot_callback);
+void robot_callback(const geometry_msgs::Twist& msg) {
+  my_robot->move(msg.linear.x, msg.angular.z);
+  double lin=msg.linear.x;
+  double ang=msg.angular.z;
+  // Calculate the right and left wheel velocities
+	double u_r = (lin + ang * 0.13/2.0) / 0.04;
+	double u_l = (lin - ang * 0.13/2.0) / 0.04;
+  lol_msg.linear.y=u_r;
+  lol_msg.angular.y=u_l;
+  lol_msg.linear.x =map(static_cast<INT_PWM>(-u_r), 0, 250, 0, 255);
+  lol_msg.angular.z = map(static_cast<INT_PWM>(-u_l), 0, 250, 0, 255);
+  lol_pub.publish(&lol_msg);
 
+}
 
-// Create a geometry_msgs::Twist object and a ROS publisher
+ros::Subscriber<geometry_msgs::Twist> sub("/turtle1/cmd_vel", &robot_callback);
 geometry_msgs::Twist cmd_msg;
 ros::Publisher cmd_pub("cmd_vel", &cmd_msg);
 
+sensor_msgs::Imu imu_msg;
+ros::Publisher imu_pub("imu", &imu_msg);
 
-/********************************** Function to handle encoder A************************************/
+std_msgs::String odom_str_msg;
+ros::Publisher raw_odom_pub("raw_odom", &odom_str_msg);
 
-void handleEncoder_A() 
-{
+std_msgs::Float64 distance_msg1;
+ros::Publisher distance_pub1("sonar", &distance_msg1);
+
+void handleEncoder_A() {
   int currentState_A = digitalRead(ENCODER_LEFT_MOTOR_A);
   int currentState_B = digitalRead(ENCODER_LEFT_MOTOR_B);
-  if ((lastEncoderState_A == LOW) && (currentState_A == HIGH)) 
-  {
-    if (currentState_B == LOW)
-    {
+  if ((lastEncoderState_A == LOW) && (currentState_A == HIGH)) {
+    if (currentState_B == LOW) {
       encoderCount_A++;
-    } 
-    else 
-    {
+    } else {
       encoderCount_A--;
     }
   }
   lastEncoderState_A = currentState_A;
 }
 
-
-/********************************** Function to handle encoder B************************************/
-
-void handleEncoder_B() 
-{
+void handleEncoder_B() {
   int currentState_A = digitalRead(ENCODER_RIGHT_MOTOR_A);
   int currentState_B = digitalRead(ENCODER_RIGHT_MOTOR_B);
-  if ((lastEncoderState_B == LOW) && (currentState_B == HIGH)) 
-  {
-    if (currentState_A == LOW)
-    {
+  if ((lastEncoderState_B == LOW) && (currentState_B == HIGH)) {
+    if (currentState_A == LOW) {
       encoderCount_B++;
-    } 
-    else 
-    {
+    } else {
       encoderCount_B--;
     }
   }
@@ -121,21 +121,18 @@ void handleEncoder_B()
 }
 
 
-/**********************************Set Up Function***********************************/
+void setup() {
+  SerialBT.begin("ESP32_BT");
+  myservo.attach(Servo_pin);
+  myservo.write(0);
 
-void setup()
-{
-  // Start the Bluetooth connection
-  SerialBT.begin("ESP32_BT"); //Bluetooth device name
-  // Initialize the robot
   my_robot = new DifferentialDriveRobot(
     new DCMotor(IN1, IN2, ENA),
-    new DCMotor(IN3, IN4,ENB));
+    new DCMotor(IN3, IN4, ENB)
+  );
 
-  // Update the robot's parameters
   my_robot->updateParameters(0.2, 1);
 
-  // Try to initialize the MPU6050
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -143,92 +140,87 @@ void setup()
     }
   }
   Serial.println("MPU6050 Found!");
-  // set accelerometer range to +-2G
   mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-  // set gyro range to +- 500 deg/s
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  // set filter bandwidth to 21 Hz
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-  // Set encoder pins as inputs
   pinMode(ENCODER_LEFT_MOTOR_A, INPUT);
   pinMode(ENCODER_RIGHT_MOTOR_A, INPUT);
   pinMode(ENCODER_LEFT_MOTOR_B, INPUT);
   pinMode(ENCODER_RIGHT_MOTOR_B, INPUT);
 
-
-  // Attach interrupts to the encoder pins
   attachInterrupt(digitalPinToInterrupt(ENCODER_LEFT_MOTOR_A), handleEncoder_A, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENCODER_LEFT_MOTOR_B), handleEncoder_A, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENCODER_RIGHT_MOTOR_A), handleEncoder_B, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENCODER_RIGHT_MOTOR_B), handleEncoder_B, CHANGE);
 
-
-  // Initialize the ROS node, advertise the publisher, and subscribe to the subscriber
   nh.initNode();
   nh.advertise(cmd_pub);
+  nh.advertise(imu_pub);
+  nh.advertise(distance_pub1);
+  nh.advertise(raw_odom_pub);
+  nh.advertise(lol_pub);
   nh.subscribe(sub);
-
 }
 
-/**********************************Main Function**********************************/
-void loop() 
-{ 
-
-  // Update the current time 
+void loop() {
   currentMillis = millis();
 
-  //get mpu readings
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-
- // If the interval has passed
- if(currentMillis - previousMillis >= interval)
- {
-  if(int(a.acceleration.x*10)>-10 && int(a.acceleration.x*10)<10 && int(a.acceleration.y*10)>-10 && int(a.acceleration.y*10)<10)
-  {
-    cmd_msg.linear.x = 0;
+  if (currentMillis - previousMillis >= interval2) {
+    float distance = ultrasonic1.read();
+    String distanceString = String(distance, 10);  
+    distance_msg1.data = distanceString.toFloat();
+    //distance_msg1.data = distance;
+    distance_pub1.publish(&distance_msg1);
+    if (flag == 0) {
+      angle += 1;
+      myservo.write(angle);
+      if (angle >= 180) {
+        flag = 1;
+      }
+    } else if (flag == 1) {
+      angle -= 1;
+      myservo.write(angle);
+      if (angle == 0) {
+        flag = 0;
+      }
+    }
   }
-  else{
-    // Calculate the angular velocities of the encoders
-    encoder_angular_A=(encoderCount_A*2*PI)/encoder_cpr;
-    encoder_angular_B=(encoderCount_B*2*PI)/encoder_cpr;
 
-    // Convert the angular velocities to a linear velocity and an angular velocity
-    encoder_linearVelocity = (wheelRadius / 2) * (encoder_angular_A + encoder_angular_B);
+  if (currentMillis - previousMillis >= interval) {
+    float encoder_angular_A = (encoderCount_A * 2 * PI) / encoder_cpr;
+    float encoder_angular_B = (encoderCount_B * 2 * PI) / encoder_cpr;
+    float encoder_linearVelocity = (wheelRadius / 2) * (encoder_angular_A + encoder_angular_B);
 
-    // Update the cmd_msg object and publish it
-    cmd_msg.linear.x=encoder_linearVelocity*3;
+    String odom_data = "ENC_A:" + String(encoder_angular_A, 4) + 
+                       ",ENC_B:" + String(encoder_angular_B, 4) + 
+                       ",VEL:" + String(encoder_linearVelocity, 4) + 
+                       ",GYRO_Z:" + String(g.gyro.z, 4);
+    odom_str_msg.data = odom_data.c_str();
+    raw_odom_pub.publish(&odom_str_msg);
 
+    encoderCount_A = 0;
+    encoderCount_B = 0;
 
+    imu_msg.header.stamp = nh.now();
+    imu_msg.header.frame_id = "imu_link";
+    imu_msg.orientation_covariance[0] = -1;
+    imu_msg.angular_velocity.x = g.gyro.x;
+    imu_msg.angular_velocity.y = g.gyro.y;
+    imu_msg.angular_velocity.z = g.gyro.z;
+    imu_msg.linear_acceleration.x = a.acceleration.x;
+    imu_msg.linear_acceleration.y = a.acceleration.y;
+    imu_msg.linear_acceleration.z = a.acceleration.z;
+
+    
+    imu_pub.publish(&imu_msg);
+
+    previousMillis = currentMillis;
   }
-    //publishe angular velocity from imu
-    cmd_msg.angular.z = g.gyro.z-0.05+0.019;
 
-    //publish message
-    cmd_pub.publish(&cmd_msg);
-    
-    // Reset the encoder counts
-    encoderCount_A=0;
-    encoderCount_B=0;
-
-    // Update the previous time
-    previousMillis = currentMillis; 
-    
- }
-
-  // Spin the ROS node once and delay for 1 ms
   nh.spinOnce(); 
   delay(1);
-}
-
-
-// Robot callback function
-void robot_callback(const geometry_msgs::Twist& msg) 
-{
-
-  // Move the robot
-	my_robot->move(msg.linear.x, msg.angular.z);
-  
 }
